8c22d47cfb4025106d784fedd436bc38afaa69a0
Paulo Alberto Ramos <pauloalbertoramos@MacBook-Pro-de-Paulo.local>
2019-11-16 12:21:39 -0300

    add vue router

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Home from './components/home/home.vue';
+
+//Quando se exporta uma variável ela tem que ser uma constante.
+export const routes = [ 
+    {
+        path: '',
+        component: Home
+    }
+];
