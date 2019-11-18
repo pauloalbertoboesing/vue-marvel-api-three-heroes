44ceabf8b71f75e5f17655b0adb3b104f9633613
Paulo Ramos <pauloalberto@outlook.com>
2019-11-18 14:42:27 -0300

    add header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
+    <header>
+      <div class="logo"></div>
+      <div class="heroes"></div>
+    </header>
     <router-view></router-view>
+    <footer>
+      <div class="heroes"></div>
+      <div class="developed">
+          Developed by Paulo Ramos, mail: pauloalberto@outlook.com, Phone: +5551 99864.35.46
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -9,7 +19,6 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
@@ -22,7 +31,36 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  header {
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: #D32A1C;
+    box-shadow: 0px 2px 5px 0px rgba(0,0,0,0.75);
+    margin-bottom: 6px;
+    .logo {
+      background: url('http://pluspng.com/img-png/marvel-png-marvel-png-497.png') no-repeat;
+      background-size: cover;
+      width: 150px;
+      height: 50px;
+    }
+  }
+
+  footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+    .heroes {
+      background: url('http://pluspng.com/img-png/marvel-png-download-1153.png') no-repeat;
+      background-size: cover;
+      width: 150px;
+      height: 50px;
+      flex: 1
+    }
+  }
 }
 
 h1, h2 {
